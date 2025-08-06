57df8cf373f4c145cb3d453e6d164e0da700feda
Avi Sharma <avisharma5016@gmail.com>
2025-08-06 11:58:20 +0530

    Matrix_input_output.cpp

--- Matrix_input_output.cpp
+++ Matrix_input_output.cpp
@@ -1,34 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int arr[2][2];
-    cout << "Enter elements of a 2x2 matrix(row-wise):" << endl;
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            cin>> arr[i][j];
-        }
-        cout<<endl;
+int main() {
+    const int MAX = 100; 
+    int rows, columns;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> columns;
+
+    if (rows > MAX || columns > MAX) {
+        cout << "Size too big!\n";
+        return 1;
     }
+
+    int arr[MAX][MAX];
+
+    cout << "Enter elements of the matrix (row-wise):" << endl;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            cin >> arr[i][j];
+
     cout << "The matrix is:" << endl;
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++)
             cout << arr[i][j] << " ";
-        }
         cout << endl;
     }
+
     return 0;
 }
 
-
 /*OUTPUT
-Enter elements of a 2x2 matrix(row-wise):
+Enter the number of rows: 2 
+Enter the number of columns: 2
+Enter elements of the matrix (row-wise):
 1 2
-
 3 4
-
 The matrix is:
 1 2
 3 4
-
 */
